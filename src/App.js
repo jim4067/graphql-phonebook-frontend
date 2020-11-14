21c3d5c8f520efa95adc834637a503a3859f0d52
jim4067 <jimmyimpulse2@gmail.com>
2020-11-14 23:45:38 +0300

    feat: notifying the user when error occurs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import { ALL_PERSONS } from './queries';
 
+const Notify = ({ errorMessage }) => {
+	if (!errorMessage) {
+		return null;
+	}
+
+	return (
+		<div style={{ color: 'red' }}  >
+			{errorMessage}
+		</div>
+	);
+}
 
 const App = () => {
+	const [errorMessage, setErrorMessage] = useState(null);
+
 	const result = useQuery(ALL_PERSONS,/* {
 		the code below makes endless queries when a new person is added
 		pollInterval: 2000
 	} */ );
+
+	const notify = (message) => {
+		setErrorMessage(message);
+		setTimeout(() => {
+			setErrorMessage(null);
+		}, 1000)
+	}
+
 
 	if (result.loading) {
 		return <div>loading...</div>
@@ -18,8 +39,9 @@ const App = () => {
 
 	return (
 		<div>
+			<Notify errorMessage={errorMessage} />
 			<Persons persons={result.data.allPersons} />
-			<PersonForm />
+			<PersonForm setError={notify} />
 		</div>
 	);
 }
