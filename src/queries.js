478c3780f181043ce3e171be9f8b2d98992d9c67
jim4067 <jimmyimpulse2@gmail.com>
2020-11-15 15:22:02 +0300

    feat: created a query for editing the phone number

--- src/queries.js
+++ src/queries.js
@@ -1,25 +1,5 @@
 import { gql } from '@apollo/client';
 
-
-export const CREATE_PERSON = gql`
-    mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String){
-        addPerson(
-            name: $name
-            street: $street
-            city: $city
-            phone: $phone
-        ) {
-            name
-            phone
-            id
-            address{
-                street
-                city
-            }
-        }
-    }
-`;
-
 export const ALL_PERSONS = gql`
   query  {
     allPersons  {
@@ -40,6 +20,39 @@ export const FIND_PERSON = gql`
 				street
 				city
 			}
+		}
+	}
+`;
+
+export const CREATE_PERSON = gql`
+    mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String){
+        addPerson(
+            name: $name
+            street: $street
+            city: $city
+            phone: $phone
+        ) {
+            name
+            phone
+            id
+            address{
+                street
+                city
+            }
+        }
+    }
+`;
+
+export const EDIT_NUMBER = gql`
+	mutation editNumber($name: String!, $phone: String!){
+		editNumber(name: $name, phone: $phone){
+			name
+			phone
+			address{
+				street
+				city
+			}
+			id
 		}
 	}
 `;
